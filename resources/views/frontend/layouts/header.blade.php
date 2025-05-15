084bfcc39d7ddc59e15a9006490e0a87f6db21d8
Boneloj7 <juansebastianbg29@gmail.com>
2025-05-15 15:53:33 -0500

    Celebrando la victoria, añadiendo el aplicativo funcionando

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -51,7 +51,10 @@
                         @php
                             $settings=DB::table('settings')->get();
                         @endphp                    
-                        <a href="{{route('home')}}"><img src="@foreach($settings as $data) {{$data->logo}} @endforeach" alt="logo"></a>
+                        <a href="{{ route('home') }}">
+                            <img src="{{ asset($settings->first()->logo) }}" alt="logo">
+                        </a>
+
                     </div>
                     <!--/ End Logo -->
                     <!-- Search Form -->
