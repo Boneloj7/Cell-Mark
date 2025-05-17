f29a01904bd7d2ff3e36b29f0610353a91987ebb
Boneloj7 <juansebastianbg29@gmail.com>
2025-05-17 11:39:59 -0500

    modifique login y agregue img

--- resources/views/frontend/pages/login.blade.php
+++ resources/views/frontend/pages/login.blade.php
@@ -26,15 +26,15 @@
             <div class="row"> 
                 <div class="col-lg-6 offset-lg-3 col-12">
                     <div class="login-form">
-                        <h2>Login</h2>
-                        <p>Please register in order to checkout more quickly</p>
+                        <h2>Cell-Mark</h2>
+                        <p>Renueva tu tecnología, ahorra y cuida el planeta: Celulares de segunda mano con calidad garantizada</p>
                         <!-- Form -->
                         <form class="form" method="post" action="{{route('login.submit')}}">
                             @csrf
                             <div class="row">
                                 <div class="col-12">
                                     <div class="form-group">
-                                        <label>Your Email<span>*</span></label>
+                                        <label>Tu correo electrónico<span>*</span></label>
                                         <input type="email" name="email" placeholder="" required="required" value="{{old('email')}}">
                                         @error('email')
                                             <span class="text-danger">{{$message}}</span>
@@ -43,7 +43,7 @@
                                 </div>
                                 <div class="col-12">
                                     <div class="form-group">
-                                        <label>Your Password<span>*</span></label>
+                                        <label>Tu Contraseña<span>*</span></label>
                                         <input type="password" name="password" placeholder="" required="required" value="{{old('password')}}">
                                         @error('password')
                                             <span class="text-danger">{{$message}}</span>
@@ -53,19 +53,15 @@
                                 <div class="col-12">
                                     <div class="form-group login-btn">
                                         <button class="btn" type="submit">Login</button>
-                                        <a href="{{route('register.form')}}" class="btn">Register</a>
-                                        OR
-                                        <a href="{{route('login.redirect','facebook')}}" class="btn btn-facebook"><i class="ti-facebook"></i></a>
-                                        <a href="{{route('login.redirect','github')}}" class="btn btn-github"><i class="ti-github"></i></a>
-                                        <a href="{{route('login.redirect','google')}}" class="btn btn-google"><i class="ti-google"></i></a>
-
+                                        <a href="{{route('register.form')}}" class="btn">Registro</a>
+                                        
                                     </div>
                                     <div class="checkbox">
-                                        <label class="checkbox-inline" for="2"><input name="news" id="2" type="checkbox">Remember me</label>
+                                        <label class="checkbox-inline" for="2"><input name="news" id="2" type="checkbox">Acuérdate de mí</label>
                                     </div>
                                     @if (Route::has('password.request'))
                                         <a class="lost-pass" href="{{ route('password.request') }}">
-                                            Lost your password?
+                                            ¿Olvidaste tu contraseña?
                                         </a>
                                     @endif
                                 </div>
@@ -82,9 +78,11 @@
 @push('styles')
 <style>
     .shop.login .form .btn{
-        margin-right:0;
+        margin-left: 110px;
+        
     }
-    .btn-facebook{
+
+    /* .btn-facebook{
         background:#39579A;
     }
     .btn-facebook:hover{
@@ -103,6 +101,6 @@
     }
     .btn-google:hover{
         background:rgb(243, 26, 26) !important;
-    }
+    } */
 </style>
 @endpush
